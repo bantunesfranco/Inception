bda50eb4596252c366f5305d60e2738e3fab1929
Bruno Franco <94724432+bantunesfranco@users.noreply.github.com>
2023-09-12 22:08:50 +0000

    added some congig stuff

--- Makefile
+++ Makefile
@@ -2,11 +2,13 @@
 all : up
 
 up :
-	mkdir -p /workspaces/codespaces-blank/Inception/db
-	mkdir -p /workspaces/codespaces-blank/Inception/wp
+	@mkdir -p /home/bfranco/data/db
+	@mkdir -p /home/bfranco/data/wp
 	@docker-compose -f ./srcs/docker-compose.yml up -d
 
 down :
+	@rm -rf /home/bfranco/data/db
+	@rm -rf /home/bfranco/data/wp
 	@docker-compose -f srcs/docker-compose.yml down
 
 stop :
@@ -20,6 +22,11 @@ build :
 
 rebuild :
 	@docker-compose -f srcs/docker-compose.yml up --build -d
+
+prune:
+	@rm -rf /home/bfranco/data/db
+	@rm -rf /home/bfranco/data/wp
+	@docker system prune -af
 
 ssh:
 	echo 1234 | ssh bfranco@localhost -p 2222
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -8,13 +8,17 @@ apt-get update -y && apt-get upgrade -y \
 
 wget https://wordpress.org/wordpress-6.3.tar.gz -P /var/www/
 
-cd /var/www && tar -xvf wordpress-6.3.tar.gz && rm -rf wordpress-6.3.tar.gz \
-&& mv /var/www/wordpress /var/www/html
+cd /var/www && tar -xvf wordpress-6.3.tar.gz
+rm -rf wordpress-6.3.tar.gz
+mv /var/www/wordpress /var/www/html
 
 chown -R root:root /var/www/html
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
+
+rm -rf /var/www/html/wp-config-sample.php
+mv /wp-config.php /var/www/html/wp-config.php
 
 #Configuring Wordpress
 
@@ -30,19 +34,19 @@ sed -i -r "s/db1/$DB_NAME/1"		wp-config.php
 sed -i -r "s/user/$DB_USER/1"		wp-config.php
 sed -i -r "s/pwd/$DB_PASSWORD/1"	wp-config.php
 
-wp core install --allow-root
+wp core install --url=$DOMAIN_NAME/ \
+ --title=$WP_TITLE \
+ --admin_user=$WP_ADMIN_USR \
+ --admin_password=$WP_ADMIN_PWD \
+ --admin_email=$WP_ADMIN_EMAIL \
+ --skip-email --allow-root
 
 #Creating Wordpress User
 
 echo "Creating Wordpress User";
-wp user create $WP_USER \
+wp user create $WP_USER $WP_USER_EMAIL \
  --user_pass=$WP_USER_PASSWORD \
  --path=$WP_PATH
 
 echo "Starting Wordpress";
 exec "$@"
-
-rm -rf /var/www/html/wp-config-sample.php
-
-mv /wp-config.php /var/www/html/wp-config.php
-
