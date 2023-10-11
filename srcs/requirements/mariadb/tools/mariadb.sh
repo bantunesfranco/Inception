7692e322944e26fd425d592a90c594cb70994384
Bruno Franco <bfranco@student.codam.nl>
2023-10-11 17:25:21 +0200

    works almost?

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,33 +2,17 @@
 
 #Starting MariaDB
 
-service mysql start;
-
-# Healthcheck
-if service mysql status >/dev/null 2>&1; then
-	echo "MariaDB is running."
-else
-	echo "MariaDB is not running. Restarting..."
-	service mysql restart
-
-	# Check again after restarting
-	if service mysql status >/dev/null 2>&1; then
-		echo "MariaDB is running."
-	else
-		echo "MariaDB is not running. Exiting..."
-		exit 1
-	fi
-
-	echo "MariaDB is running."
-fi
+service mariadb start;
 
 # Creating database
 
+echo "SET PASSWORD FOR 'root'@'%' = PASSWORD('$DB_ROOT_PASSWORD');" >> db-setup.sql
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db-setup.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost';" >> db-setup.sql
-echo "FLUSH PRIVILEGES;" >> db-setup.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';" >> db-setup.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
+echo "ALTER USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
+
+mysql < db-setup.sql
 
 exec "$@"
 
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -9,13 +9,10 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
 	wp core download --allow-root --path=/var/www/html --version=$WP_VERSION --locale=$WP_LOCALE
 
-	wp config create --allow-root --dbname=$DB_NAME \
-	 --dbuser=$DB_USER --dbpass=$DB_PASSWORD \
-	 --dbhost=$DB_HOST --dbcharset="utf8" \
-	 --dbcollate="utf8_general_ci"
+	wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD \
+	 --dbhost=$DB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --config-file=/var/www/html/wp-config.php
 
-	wp core install --allow-root --url=$DOMAIN_NAME \
-	 --title=$WP_TITLE --admin_user=$WP_ADMIN_USER \
+	wp core install --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER \
 	 --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL \
 	 --skip-email --skip-plugins --skip-themes
 
