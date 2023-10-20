d147cce827d0c5e1ea30355b200edb95e23d349e
bantunesfranco <brunoantunesfranco@gmail.com>
2023-10-20 12:18:57 +0200

    adminer works

--- srcs/requirements/bonus/adminer/tools/adminer.sh
+++ srcs/requirements/bonus/adminer/tools/adminer.sh
@@ -2,13 +2,14 @@
 
 mkdir -p /run/php/
 
-curl -O https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php
-
-cat adminer-4.8.1.php
+wget -O adminer.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php
 
 mkdir -p /var/www/html
-chmod 777 adminer-4.8.1.php
-mv adminer-4.8.1.php /var/www/html/index.php
+ls -la /var/www/html
+
+chmod 777 ./adminer.php
+mv ./adminer.php /var/www/html/index.php
+chown -R www-data:www-data /var/www/html
 
 echo "Adminer is ready!"
 exec "$@"
