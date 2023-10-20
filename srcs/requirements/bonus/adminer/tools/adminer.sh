e74d024c7acbec94240e9047cb10f44cf73c79ca
bantunesfranco <brunoantunesfranco@gmail.com>
2023-10-20 13:14:47 +0200

    fixed weird db restart

--- srcs/requirements/bonus/adminer/tools/adminer.sh
+++ srcs/requirements/bonus/adminer/tools/adminer.sh
@@ -5,7 +5,6 @@ mkdir -p /run/php/
 wget -O adminer.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php
 
 mkdir -p /var/www/html
-ls -la /var/www/html
 
 chmod 777 ./adminer.php
 mv ./adminer.php /var/www/html/index.php
--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-# mkdir -p /run/mysqld
-# touch /run/mysqld/mysqld.sock
-# chmod 777 /run/mysqld/mysqld.sock
-
-#Starting MariaDB
-service mariadb start;
-
 #Creating database
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db-setup.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
 echo "ALTER USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
-# echo "ALTER USER 'root'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
-# echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';" >> db-setup.sql
-# echo "FLUSH PRIVILEGES;" >> db-setup.sql
-
-mysql < db-setup.sql
 
 echo "MariaDB is ready";
-exec "$@"
+exec "$@" < db-setup.sql
 
