6d07b0edeb967b3c4b87067b0ae77509020fbf76
bantunesfranco <brunoantunesfranco@gmail.com>
2023-10-25 15:56:43 +0200

    fixed database install

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 #Creating database
-if [ ! -f db-setup.sql ]; then
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db-setup.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
 echo "ALTER USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
-fi
+echo "FLUSH PRIVILEGES;" >> db-setup.sql
+
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db --skip-name-resolve --auth-root-authentication-method=normal
+
+service mariadb start
+mysql < db-setup.sql
+service mariadb stop
 
 echo "MariaDB is ready";
-exec "$@" < db-setup.sql
-
+exec "$@" 
