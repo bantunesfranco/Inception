82818c9fbcbd9e6a13ecf7dc4bfb4a0027e046e9
bantunesfranco <brunoantunesfranco@gmail.com>
2023-10-25 19:28:56 +0200

    works - added start to static page

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 #Creating database
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db-setup.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
-echo "ALTER USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
-echo "FLUSH PRIVILEGES;" >> db-setup.sql
 
-mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db --skip-name-resolve --auth-root-authentication-method=normal
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
-service mariadb start
-mysql < db-setup.sql
-service mariadb stop
+    if [ ! -f db-setup.sql ]; then
+        echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db-setup.sql
+        echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
+        echo "ALTER USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
+        echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db-setup.sql
+        echo "FLUSH PRIVILEGES;" >> db-setup.sql
+    fi
+
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db --skip-name-resolve --auth-root-authentication-method=normal
+
+    service mariadb start
+    mysql < db-setup.sql
+    service mariadb stop
+fi
 
 echo "MariaDB is ready";
 exec "$@" 
